b2cbfc81c5e4fda29fe2055f756a012cb7a15649
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-10-25 14:14:20 +0400

    Added empty ShellSort and test for it.

--- csharp/AlgorithmTest.Test/ShellSortTest.cs
+++ csharp/AlgorithmTest.Test/ShellSortTest.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace AlgorithmTest.Test
 {
     [TestFixture]
-    public class MergeSortTest : SortingTest
+    public class ShellSortTest : SortingTest
     {
         private static readonly int[][] ToSort = ToSortBase;
 
         [TestCaseSource("ToSort")]
         public void SimpleCases(ICollection<int> paramCollection)
         {
-            var result = MergeSort.Sort(paramCollection);
+            var result = ShellSort.Sort(paramCollection);
             Assert.That(result, MoreThan.Sorted<int>());
         }
     }

e8d609a3b6a180ee6e23277dc9d780609daac9b2
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-10-26 15:56:17 +0300

    Added StreamSelection. It returns k-th largest number from the stream.

---
+++ csharp/AlgorithmTest/StreamSelection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmTest
+{
+    public class StreamSelection
+    {
+        public static T Select<T>(IEnumerable<T> source, int k, Func<T, T, int> comparator)
+        {
+            k = k + 1;
+            var buffer = new T[k + 1];
+            var size = 0;
+
+            foreach (var item in source)
+            {
+                buffer[size] = item;
+
+                for (var i = size; i > 0 && comparator(buffer[i], buffer[i - 1]) < 0; --i)
+                {
+                    buffer.Swap(i, i - 1);
+                }
+
+                if (++size > k)
+                    size = k;
+            }
+
+            return buffer[k - 1];
+        }
+
+        public static T Select<T>(IEnumerable<T> source, int k) where T : IComparable<T>
+        {
+            return Select(source, k, (x, y) => x.CompareTo(y));
+        }
+    }
+}

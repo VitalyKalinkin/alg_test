dc83556aa94878fc14c019bee6fd7695c633dd9a
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-11-01 12:15:58 +0300

    Added BreadthFirstSeach to split tree by layers.

---
+++ csharp/AlgorithmTest.Test/BreadthFirstSearchTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AlgorithmTest.Test
+{
+    [TestFixture]
+    public class BreadthFirstSearchTest
+    {
+        [Test]
+        public void SimpleTree()
+        {
+            var root = GenerateTree();
+            var actualLevels = BreadthFirstSearch<int>.SplitByLevels(root);
+            Assert.That(actualLevels.Select(x => string.Join(",", x.Select(y => y.Payload))),
+                Is.EqualTo(new[] {"0", "1,2,3", "4,5,8", "6,7"}));
+        }
+
+        private static GeneralTreeNode<int> GenerateTree()
+        {
+            return new GeneralTreeNode<int>(0, new[]
+            {
+                new GeneralTreeNode<int>(1, new[]
+                {
+                    new GeneralTreeNode<int>(4), 
+                    new GeneralTreeNode<int>(5, new[]
+                    {
+                        new GeneralTreeNode<int>(6), 
+                        new GeneralTreeNode<int>(7)
+                    }), 
+                }),
+                new GeneralTreeNode<int>(2, new[]
+                {
+                    new GeneralTreeNode<int>(8) 
+                }), 
+                new GeneralTreeNode<int>(3) 
+            });
+        }
+    }
+}
---
+++ csharp/AlgorithmTest/BreadthFirstSearch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AlgorithmTest
+{
+    public static class BreadthFirstSearch<T>
+    {
+        public static IEnumerable<IEnumerable<GeneralTreeNode<T>>> SplitByLevels(GeneralTreeNode<T> root)
+        {
+            var bfsQueue = new Queue<GeneralTreeNode<T>>();
+            var currentLevel = new List<GeneralTreeNode<T>>();
+
+            bfsQueue.Enqueue(root);
+            bfsQueue.Enqueue(GeneralTreeNode<T>.Empty);
+
+            while (bfsQueue.Count > 0)
+            {
+                var item = bfsQueue.Dequeue();
+
+                if (item == GeneralTreeNode<T>.Empty)
+                {
+                    yield return currentLevel;
+                    currentLevel = new List<GeneralTreeNode<T>>();
+                    
+                    if (bfsQueue.Count > 0)
+                        bfsQueue.Enqueue(GeneralTreeNode<T>.Empty);
+
+                    continue;
+                }
+
+                currentLevel.Add(item);
+
+                foreach (var childNode in item.ChildNodes)
+                {
+                    bfsQueue.Enqueue(childNode);
+                }
+            }
+        }
+    }
+}
